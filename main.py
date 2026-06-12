83143ea82ea3d174d12362bdf9a4b860b62c4f52
akuunoro <marabilla.rr.bscs@gmail.com>
2026-06-13 05:42:37 +0800

    Playground: Learning How Caching, Image Load, and Blit vs Draw
    Refer to akuunoro_06.08.2026.md for details

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
-#akuunoro
+# akuunoro
 import pygame
+from src.assets_cache.player_cache import load_player_sprite
 
 # SETUP
-pygame.init() #initialize all pygame modules
-screen = pygame.display.set_mode((1280, 720)) # tuple for display screen (width,pixel)
+pygame.init()  # initialize all pygame modules
+# tuple for display screen (width,pixel)
+screen = pygame.display.set_mode((1280, 720))
 
+pygame.display.set_caption("Test Game by akuunoro")  # setting caption
 
-pygame.display.set_caption("Test Game by akuunoro") #setting caption
+# LOAD ASSETS
+player = load_player_sprite("Knight (Front)1.png")
 
-running = True#initialize running to TRUE for game loop
+running = True  # refer to akuunoro_06.08.2026.md - section 1.1
 
 # SET GAME LOOP TO RUN
 while running:
-    for event in pygame.event.get(): #this gets every events or inputs and put it in a queue to be act upon
-        if event.type == pygame.QUIT:  # click x and close, basically a quit event
-            running = False #stops the game on the next loop check
-    
-    #the bg
-    screen.fill((0, 0, 0)) #can be pygame color set or rgb values by implementing a tuple
+    for event in pygame.event.get():  # refer to akuunoro_06.08.2026.md - section 1.2
+        if event.type == pygame.QUIT:  # refer to akuunoro_06.08.2026.md - section 1.3
+            running = False  # refer to akuunoro_06.08.2026.md - section 1.4
+
+    # the bg
+    screen.fill((0, 0, 0))  # refer to akuunoro_06.08.2026.md - section 2
+    clock = pygame.time.Clock()
 
     # RENDER GAME
-   
-    #idk if thisis right, but this is how I understand it
-    pygame.display.flip() #basically this one, what it does is update the screen using a buffer (front and back). 
-    """The front buffer is the screen that is display and can be seen by the player, while the back buffer is the one that
-    creates or draw the update. Once the pyGame or back buffer is done, it will be sent to the front buffer so it can be shown"""
+    screen.blit(player, (5, 5))  # refer to akuunoro_06.08.2026.md - section 5
+
+    # idk if this is right, but this is how I understand it
+    pygame.display.flip()  # refer to akuunoro_06.08.2026.md - section 3
+    clock.tick(60)
 
 
-pygame.quit() #just like closing a db connection or scanner in java using 'close()'. This one use 'quit()' to completely close the pygame engine
+pygame.quit()  # refer to akuunoro_06.08.2026.md - section 4
