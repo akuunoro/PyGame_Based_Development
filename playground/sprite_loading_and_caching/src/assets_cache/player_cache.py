2feb69e790fd6dddaff3f5df921798ee608bb7bf
akuunoro <marabilla.rr.bscs@gmail.com>
2026-06-13 09:24:56 +0800

    Playground: Fix Comments
    Forget to change the comments to link to the devlog#2

--- playground/sprite_loading_and_caching/src/assets_cache/player_cache.py
+++ playground/sprite_loading_and_caching/src/assets_cache/player_cache.py
@@ -2,22 +2,22 @@ import pygame
 from directory_settings import PLAYGROUND_ROOT
 
 
-sprite_cache = {} #dictionary to store the sprites that we will cache
+sprite_cache = {} #refer to akuunoro_06.08.2026.md - section 1.1
 
 def load_player_sprite(load_filename, load_size=None):
-  sprite_settings = (load_filename, load_size)  #this will act as a key for the dictionary, while its contents inside will be the value of this key
+  sprite_settings = (load_filename, load_size) #refer to akuunoro_06.08.2026.md - section 1.2
 
   if sprite_settings not in sprite_cache:
 
     sprite = pygame.image.load(
-      f"{PLAYGROUND_ROOT}/assets/sprites/players/{load_filename}" #apparently, the pygame still works in the parent directory. So i need to backtrack to it.
+      f"{PLAYGROUND_ROOT}/assets/sprites/players/{load_filename}" #refer to akuunoro_06.08.2026.md - section 1.3
     ).convert_alpha()
 
-    if load_size: #wether there's a size set or not, if there's not. Ignore this.
+    if load_size: #refer to akuunoro_06.08.2026.md - section 1.5
       sprite = pygame.transform.scale(
         sprite, load_size
       )
     
-    sprite_cache[sprite_settings] = sprite #set it in the dictionary
+    sprite_cache[sprite_settings] = sprite #refer to akuunoro_06.08.2026.md - section 1.6
 
   return sprite_cache[sprite_settings]
