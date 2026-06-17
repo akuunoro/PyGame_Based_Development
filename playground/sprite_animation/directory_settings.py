98f0cb7dfd9a058f3f280b0356bfcfe2c830d679
akuunoro <marabilla.rr.bscs@gmail.com>
2026-06-18 05:55:11 +0800

    Playground: Exploring Sprite Animation
    tbh, I don't have any idea how do I make animation here in Pygame, do I just add the sprite sheet and call it a day? IDK

---
+++ playground/sprite_animation/directory_settings.py
@@ -0,0 +1,7 @@
+#set directory
+from pathlib import Path
+
+PLAYGROUND_ROOT = Path(__file__).resolve().parent
+
+
+
---
+++ playground/sprite_animation/main.py
@@ -0,0 +1,29 @@
+#akuunoro
+import pygame 
+
+#SETUP
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+
+pygame.display.set_caption("Playground: Sprite Animation")
+
+#LOAD ASSETS
+
+
+#GAMEPLAY LOOP
+running = True
+
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+
+  screen.fill((195, 177, 225))
+  clock = pygame.time.Clock()
+
+  #RENDER 
+
+  pygame.display.flip()
+  clock.tick(60)
+
+pygame.quit()
